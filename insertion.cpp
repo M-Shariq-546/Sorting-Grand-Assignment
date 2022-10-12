28a7660472b1ff3b16697312ced11e2f71f8fd70
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-10-12 06:14:53 +0500

    Insertion Sort

--- insertion.cpp
+++ insertion.cpp
@@ -27,7 +27,7 @@ void printFinal(int arr[] , int d)
 }
 int main()
 {
-    int arr[] = {12 , 98 , 45 , 100 , 23 , 420 , 544 , 578 , 500 , 1002};
+    int arr[] = {5 , 4 , 6 , 0 , 4 , 5 , 2 , 2 , 6};
     int size = sizeof(arr)/sizeof(arr[0]);
     insertion(arr , size);
     printFinal(arr , size);
