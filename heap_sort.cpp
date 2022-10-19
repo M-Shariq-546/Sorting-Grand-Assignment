22c6ec5fa67816a9c5ab4f56cc2e5ce733c45bfd
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-10-19 11:44:32 +0500

    Heap Sort

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -1,50 +1,61 @@
-// C++ program for implementation of Heap Sort
-
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-// To heapify a subtree rooted with node i
-// which is an index in arr[].
-// n is size of heap
-void heapify(int arr[], int N, int i)
+void heapify(int arr[] , int N , int i)
 {
-	int largest = i; // Initialize largest as root
-	int left = 2 * i + 1; // Left value
-    int right = 2 * i + 2; //Right value
-	if (left < N && arr[left] > arr[largest]) // largest left case
-		largest = left;
+    int large = i;
 
-	if (right < N && arr[right] > arr[largest])//largest right case
-		largest = right;
+    int left = 2 * i + 1;
 
-	if (largest != i)//Not the root value
+    int right = 2 * i + 2;
+
+    if(left<N && arr[left] > arr[large])
+        large = left;
+
+    if(right<N && arr[right] > arr[large])
+        large = right;
+
+    if(large != i)
     {
-		swap(arr[i], arr[largest]);
-		heapify(arr, N, largest);//heapify recursion
-	}
+        swap(arr[i] , arr[large]);
+
+        heapify(arr , N , large);
+    }
+
 }
-void heapSort(int arr[], int N)
+void swap(int x , int y)
 {
-	for (int i = N / 2 - 1; i >= 0; i--)
-		heapify(arr, N, i);
-	for (int i = N - 1; i > 0; i--)
+
+}
+void heapSort(int arr[] , int N)
+{
+    for(int i = N/2-1; i>=0; i--)
     {
-		swap(arr[0], arr[i]);
-		heapify(arr, i, 0);
-	}
+        heapify(arr , N , i);
+    }
+    for(int i=N-1 ; i>=0 ; i--)
+    {
+        swap(arr[0] , arr[i]);
+
+        heapify(arr , N , 0);
+    }
 }
-void printArray(int arr[], int N)
+void print(int arr[] , int N)
 {
-    cout<<"The Sorted Array (Heap Sort) is : ";
-	for (int i = 0; i < N; ++i)
-		cout << arr[i] << "\t";
-	cout << endl;
+    for(int i=0; i<N ; i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+    cout<<"\n";
 }
 int main()
 {
-	int arr[] = {5 , 4 , 6 , 0 , 4 , 5 , 2 , 2 , 6};
-	int size = sizeof(arr) / sizeof(arr[0]);
-	heapSort(arr, size);
-	printArray(arr, size);
+    int arr[] ={5 , 4 , 6 , 0 , 4 , 5 , 2 , 2 , 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    heapSort(arr , size);
+
+
+    print(arr , size);
+
     return 0;
 }
